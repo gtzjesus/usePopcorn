ad9224539fc4faa33fc51e6197ba971f5a536156
gtz.jesus <gtz.jesus@outlook.com>
2023-08-09 12:37:39 -0600

    Creating Stars

--- src/App.js
+++ src/App.js
@@ -61,10 +61,12 @@ export default function App() {
         <Search />
         <NumResults movies={movies} />
       </NavBar>
+
       <Main>
         <Box>
           <MovieList movies={movies} />
         </Box>
+
         <Box>
           <WatchedSummary watched={watched} />
           <WatchedMoviesList watched={watched} />
