9fa0a04d6b9998248edef185ca31ec653b226365
gtz.jesus <gtz.jesus@outlook.com>
2023-08-15 13:45:03 -0600

    Selecting DOM elements the react way

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import StarRating from './StarRating';
 
 const average = (arr) =>
@@ -149,6 +149,26 @@ function Logo() {
   );
 }
 function Search({ query, setQuery }) {
+  // useEffect(function () {
+  //   // NOT FOR REACT
+  //   // const element = document.querySelector('.search');
+  //   // element.focus();
+  // }, []);
+
+  const inputElement = useRef(null);
+
+  useEffect(function () {
+    function callback(event) {
+      if (document.activeElement === inputElement.current) return;
+      if (event.code === 'Enter') {
+        inputElement.current.focus();
+        setQuery('');
+      }
+    }
+    document.addEventListener('keydown', callback);
+    return () => document.addEventListener('keydown', callback);
+  }, []);
+
   return (
     <input
       className="search"
@@ -156,6 +176,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputElement}
     />
   );
 }
