5f21a051657d95650e8a13fc68cd3af755fc6aa3
gtz.jesus <gtz.jesus@outlook.com>
2023-08-15 16:27:51 -0600

    useLocalStorageState functionality (reusable custom hook)

--- src/useMovies.js
+++ src/useMovies.js
@@ -2,14 +2,13 @@ import { useEffect, useState } from 'react';
 
 const KEY = 'b33ea6d6';
 
-export function useMovies(query, callback) {
+export function useMovies(query) {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
 
   useEffect(
     function () {
-      callback?.();
       const controller = new AbortController();
       async function fetchMovies() {
         try {
