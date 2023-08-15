13b31604d2a729181c63940e69850551a2931751
gtz.jesus <gtz.jesus@outlook.com>
2023-08-15 16:38:34 -0600

    useKey react custom hook for key bindings

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import StarRating from './StarRating';
 import { useMovies } from './useMovies';
 import { useLocalStorageState } from './useLocalStorageState';
+import { useKey } from './useKey';
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -105,20 +106,11 @@ function Search({ query, setQuery }) {
   // SELECTING DOM ELEMENTS
   const inputElement = useRef(null);
 
-  useEffect(
-    function () {
-      function callback(event) {
-        if (document.activeElement === inputElement.current) return;
-        if (event.code === 'Enter') {
-          inputElement.current.focus();
-          setQuery('');
-        }
-      }
-      document.addEventListener('keydown', callback);
-      return () => document.addEventListener('keydown', callback);
-    },
-    [setQuery]
-  );
+  useKey('Enter', function () {
+    if (document.activeElement === inputElement.current) return;
+    inputElement.current.focus();
+    setQuery('');
+  });
 
   return (
     <input
@@ -249,21 +241,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     // setAverageRating((x) => (x + userRating) / 2);
   }
 
-  useEffect(
-    function () {
-      function callback(event) {
-        if (event.code === 'Escape') {
-          onCloseMovie();
-        }
-      }
-      document.addEventListener('keydown', callback);
-
-      return function () {
-        document.removeEventListener('keydown', callback);
-      };
-    },
-    [onCloseMovie]
-  );
+  useKey('Escape', onCloseMovie);
 
   useEffect(
     function () {
